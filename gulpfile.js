7e3334f64becf9985007e6bef0006df53ffa5b2a
Brady <mbrady@mbrady-3s-mc-01.threespot.com>
2014-01-15 11:40:11 -0500

    covert gulp file include to gulp-load-task

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,20 @@
-var gp = {}; // gulp packages
-// Load all Grunt tasks declared in the package.json file
-var dep = require('matchdep').filterDev('gulp*').forEach(function(pkg){
-	if (pkg !== "gulp") {
-		var pkgName = pkg.replace("gulp-", "");
-		gp[pkgName] = require(pkg);
-		return gp[pkgName];
-	}
-	return gp[pkg] = require(pkg);
-});
+var gulp = require('gulp');
+var gulpLoadTasks = require("gulp-load-tasks");
 
-gp.gulp.task('scripts', function() {
-	return gp.gulp
+// Load all gulp tasks
+var tasks = gulpLoadTasks();
+
+gulp.task('scripts', function() {
+	return gulp
 		.src([
 			"js/**/*.js"
 		])
-		.pipe(gp.uglify())
-		.pipe(gp.gulp.dest('build/js'));
+		.pipe(tasks.uglify())
+		.pipe(gulp.dest('gulp_build/js'));
 });
 
 	
-gp.gulp.task('default', function() {
-	gp.gulp.run('scripts');
+gulp.task('default', function() {
+	gulp.run('scripts');
 });
 
